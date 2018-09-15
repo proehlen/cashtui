d732b15583c3eb600040b92835031801f88f103c
Peter Roehlen <peter.roehlen@gmail.com>
2018-09-15 11:21:24 +0700

    Add active option to menu with cycling via arrow and exec via Enter

--- src/view/Menu.js
+++ src/view/Menu.js
@@ -7,10 +7,15 @@ import AbstractComponent from './AbstractComponent';
 import MenuOption from './MenuOption';
 import stack from './stack';
 
+const KEY_ENTER = String.fromCharCode(0x0d);
 const KEY_ESCAPE = String.fromCharCode(0x1b);
+const KEY_LEFT = String.fromCharCode(0x1b, 0x5b, 0x44);
+const KEY_RIGHT = String.fromCharCode(0x1b, 0x5b, 0x43);
+const OPTION_GAP = 2; // Render gap between options
 
 export default class Menu extends AbstractComponent {
   _options: MenuOption[]
+  _activeOption: number
 
   constructor(title: string, options: MenuOption[]) {
     super(title);
@@ -20,6 +25,8 @@ export default class Menu extends AbstractComponent {
 
     // Every menu has to allow for quitting
     this._options.push(new MenuOption('Q', 'Quit', 'Exit the program'));
+
+    this._activeOption = 0;
   }
 
   render() {
@@ -35,34 +42,65 @@ export default class Menu extends AbstractComponent {
         text: colors.cyan(preKeyText) +
           colors.cyan.bold(option.key) +
           colors.cyan(postKeyText),
-        width: option.label.length + 2,
+        width: option.label.length + OPTION_GAP,
       }
     });
     ui.div(...options);
 
     console.log(ui.toString());
-    readline.cursorTo(process.stdout, 0, 1);
+    this._moveCursorToActiveOption();
+  }
+  
+  _moveCursorToActiveOption() {
+    let x = 0;
+    for (let i = 0; i < this._activeOption; i++) {
+      const option = this._options[i];
+      x += (option.label.length + OPTION_GAP);
+    }
+    readline.cursorTo(process.stdout, x, 1);
   }
 
   handle(key: string) {
-    switch (key.toUpperCase()) {
-      case KEY_ESCAPE:
-        if (stack.depth) {
-          stack.pop();
-        } else {
+    if (key === KEY_ENTER) {
+      // Call back this method (maybe in child class) with key
+      // for active option
+      const option = this._options[this._activeOption];
+      this.handle(option.key);
+    } else {
+      switch (key.toUpperCase()) {
+        case KEY_ESCAPE:
+          if (stack.depth) {
+            stack.pop();
+          } else {
+            stack.quit();
+          }
+          break;
+        case KEY_LEFT: 
+          this._activeOption -= 1;
+          if (this._activeOption < 0) {
+            this._activeOption = this._options.length - 1;
+          }
+          stack.render();
+          break;
+        case KEY_RIGHT: 
+          this._activeOption += 1;
+          if (this._activeOption >= this._options.length) {
+            this._activeOption = 0;
+          }
+          stack.render();
+          break;
+        case 'B':
+          // Back
+          if (stack.depth) {
+            stack.pop();
+          }
+          break;
+        case 'Q':
+          // Quit
           stack.quit();
-        }
-        break;
-      case 'B':
-        // Back
-        if (stack.depth) {
-          stack.pop();
-        }
-        break;
-      case 'Q':
-        // Quit
-        stack.quit();
-        break;
+          break;
+      }
+
     }
   }
 }

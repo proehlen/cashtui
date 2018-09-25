71d405e8419a080db664d65df451fa7760a14906
Peter Roehlen <peter.roehlen@gmail.com>
2018-09-25 07:56:43 +0700

    Start view is Recent connections if user has history
    Allows for quickly reconnecting to last node

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 // @flow
 
+import Connection from './model/Connection';
 import stack from './view/stack';
 import output from './view/output';
 import NetworkSelection from './view/views/NetworkSelection';
+import ConnectionHistory from './view/views/ConnectionHistory';
 
-stack.push(new NetworkSelection());
+// Render starting view
+if (Connection.getHistory().length) {
+  stack.push(new ConnectionHistory());
+} else {
+  stack.push(new NetworkSelection());
+}
 stack.render();
 
 const { stdin } = process;
-
 // $flow-disable-line need raw mode; method appears to be available
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');

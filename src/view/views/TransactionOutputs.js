5c29a8dd0eaa59e54debf5bb340c4ec852800e3d
Peter Roehlen <peter.roehlen@gmail.com>
2018-10-01 09:54:30 +0700

    Transaction output details

--- src/view/views/TransactionOutputs.js
+++ src/view/views/TransactionOutputs.js
@@ -1,11 +1,13 @@
 // @flow
 
 import Transaction from 'cashlib/lib/Transaction';
+import Output from 'cashlib/lib/Output';
 import ViewBase from 'tooey/lib/ViewBase';
 import Menu from 'tooey/lib/Menu';
 import MenuOption from 'tooey/lib/MenuOption';
 
 import OutputsList from '../components/OutputsList';
+import TransactionOutput from './TransactionOutput';
 import TransactionAddP2PKH from './TransactionAddP2PKH';
 import state from '../../model/state';
 import app from '../app';
@@ -16,12 +18,35 @@ export default class TransactionOutputs extends ViewBase {
 
   constructor() {
     super('Transaction Outputs');
-    const addOutput = new MenuOption('A', 'Add P2PKH', 'Add new Pay To Public Key Hash output',
-      async () => app.pushView(new TransactionAddP2PKH()));
-    this._menu = new Menu(app, [addOutput]);
-
     const transaction: Transaction = state.transactions.active;
-    this._list = new OutputsList(transaction.outputs, this._menu);
+
+    const menuOptions = [
+      new MenuOption('S', 'Show', 'Show details for selected output',
+        this.toDetails.bind(this)),
+      new MenuOption('A', 'Add P2PKH', 'Add new Pay To Public Key Hash output',
+        async () => app.pushView(new TransactionAddP2PKH())),
+    ];
+    this._menu = new Menu(app, menuOptions);
+
+    this._list = new OutputsList(transaction.outputs, this._menu, true);
+  }
+
+  async toDetails() {
+    const output = state.transactions.active.outputs[this._list.selectedOutputIndex];
+    if (output) {
+      app.pushView(new TransactionOutput(
+        output,
+        state.transactions.active.getId(),
+        this._list.selectedOutputIndex,
+      ));
+    } else {
+      app.setWarning('No output selected');
+    }
+  }
+
+  getSelectedOutput(): Output {
+    const selectedIndex = this._list.selectedOutputIndex;
+    return state.transactions.active.outputs[selectedIndex];
   }
 
   render() {
